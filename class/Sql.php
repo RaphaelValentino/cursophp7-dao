19b17e8894ec1c92af8e7a761e777a7d0550f1f4
Raphael Valentino Marques de Lima <raphaelvalentino@gmail.com>
2023-03-14 22:08:39 -0300

    metodos de lista

--- class/Sql.php
+++ class/Sql.php
@@ -6,27 +6,27 @@ class Sql extends PDO {
 
     public function __construct() {
 
-        $this->conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "");
+        $this->conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "root");
 
     }
 
-    private function setParams($statment, $parameters = array()) {
+    private function setParams($statement, $parameters = array()) {
 
         foreach ($parameters as $key => $value) {
 
-            $this->setParam($statment, $key, $value);
+            $this->setParam($statement, $key, $value);
 
         }
 
     }
 
-    private function setParam($statment, $key, $value){
+    private function setParam($statement, $key, $value){
 
-        $statment->bindParam($key, $value);
+        $statement->bindParam($key, $value);
 
     }
 
-    public function execQuery($rawQuery, $params = array()){
+    public function query($rawQuery, $params = array()) {
 
         $stmt = $this->conn->prepare($rawQuery);
 
@@ -41,7 +41,7 @@ class Sql extends PDO {
     public function select($rawQuery, $params = array()):array
     {
 
-        $stmt = $this->execQuery($rawQuery, $params);
+        $stmt = $this->query($rawQuery, $params);
 
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
 
